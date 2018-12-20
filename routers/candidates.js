5b2ee005899197876dbb5d8e371e4c31747fd368
Dwyte <senshi.kasei@gmx.com>
2018-12-20 15:34:39 +0800

    Updated Candidate Route

--- routers/candidates.js
+++ routers/candidates.js
@@ -13,6 +13,37 @@ router.get('/:lrn', async (req, res) => {
     res.send(candidate);
 })
 
+router.get('/', async (req, res) => {
+    const candidates = {
+        presidents: await Candidate
+        .find({position: "President"}),
+        vicePresidents: await Candidate
+        .find({position: "Vice President"}),
+        secretaries: await Candidate
+        .find({position: "Secretary"}),
+        auditors: await Candidate
+        .find({position: "Auditor"}),
+        treasurers: await Candidate
+        .find({position: "Treasurer"}),
+        pios: await Candidate
+        .find({position: "PIO"}),
+        g7rep: await Candidate
+        .find({position: "G7 Representative"}),
+        g8rep: await Candidate
+        .find({position: "G8 Representative"}),
+        g9rep: await Candidate
+        .find({position: "G9 Representative"}),
+        g10rep: await Candidate
+        .find({position: "G10 Representative"}),
+        g11rep: await Candidate
+        .find({position: "G11 Representative"}),
+        g12rep: await Candidate
+        .find({position: "G12 Representative"})
+    }
+
+    res.send(candidates);
+});
+
 // Tally Candidates on Position
 router.get('/tally/:position', async (req, res) => {
     const candidates = await Candidate
@@ -23,7 +54,7 @@ router.get('/tally/:position', async (req, res) => {
 });
 
 // Get all candidates
-router.get('/', async (req, res) => {
+router.get('/tally', async (req, res) => {
     const candidates = {
         presidents: await Candidate
         .find({position: "President"})
@@ -42,6 +73,24 @@ router.get('/', async (req, res) => {
         .sort({votes: -1}),
         pios: await Candidate
         .find({position: "PIO"})
+        .sort({votes: -1}),
+        g7rep: await Candidate
+        .find({position: "G7 Representative"})
+        .sort({votes: -1}),
+        g8rep: await Candidate
+        .find({position: "G8 Representative"})
+        .sort({votes: -1}),
+        g9rep: await Candidate
+        .find({position: "G9 Representative"})
+        .sort({votes: -1}),
+        g10rep: await Candidate
+        .find({position: "G10 Representative"})
+        .sort({votes: -1}),
+        g11rep: await Candidate
+        .find({position: "G11 Representative"})
+        .sort({votes: -1}),
+        g12rep: await Candidate
+        .find({position: "G12 Representative"})
         .sort({votes: -1})
     }
 
