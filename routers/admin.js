7d835d7bf5720e6e590b7997700933e148ab65cd
Dwyte <senshi.kasei@gmx.com>
2018-12-20 16:15:57 +0800

    Added admin changePass put request

--- routers/admin.js
+++ routers/admin.js
@@ -3,6 +3,8 @@ const _ = require('lodash');
 const bcrypt = require('bcrypt');
 const express = require('express');
 const router = express.Router();
+const auth = require("../middleware/auth")
+const admin = require("../middleware/admin")
 
 // Register Admin
 router.post('/', async (req, res) => {
@@ -35,6 +37,21 @@ router.post('/auth', async (req, res) => {
 
     const token = generateToken();
     res.send({authToken: token});
+});
+
+router.put('/changePass', [auth, admin] , async (req, res) => {
+    // Get Admin
+    let admin = await Admin.findOne();
+
+    const validPassword = await bcrypt.compare(req.body.currentPass, admin.password);
+    if(!validPassword) return res.status(400).send('Invalid current password');
+
+    const salt = await bcrypt.genSalt(10);
+    admin.password = await bcrypt.hash(req.body.newPass, salt);
+
+    admin.save();
+
+    res.send("Password has changed!");
 });
 
 module.exports = router;
