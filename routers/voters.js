cb421436ed8bb1df800d8e5fd65f7118b7476243
Dwyte <martyymash@gmail.com>
2019-03-17 16:58:07 +0800

    Improved Voter Searching with RegEx

--- routers/voters.js
+++ routers/voters.js
@@ -1,150 +1,178 @@
-const auth = require('../middleware/auth');
-const admin = require('../middleware/admin');
-const _ = require('lodash');
-const express = require('express');
+const auth = require("../middleware/auth");
+const admin = require("../middleware/admin");
+const _ = require("lodash");
+const express = require("express");
 const router = express.Router();
-const excelToJson = require('convert-excel-to-json');
+const excelToJson = require("convert-excel-to-json");
 
 // Models
-const {Voter, validate, generateToken} = require('../models/voter');
+const { Voter, validate, generateToken } = require("../models/voter");
 
 // Import Voters
-router.post('/import', [auth, admin], async (req, res) => {
-    if(req.files){
-        var file = req.files.file0,
-            filename = file.name;
-        
-        const path = require('path');
-        if(path.extname(filename) != '.xlsx' && path.extname(filename) != '.csv')
-            return res.status(400).send({message: 'Invalid file type. Please use a .xlsx file.'})
+router.post("/import", [auth, admin], async (req, res) => {
+  if (req.files) {
+    var file = req.files.file0,
+      filename = file.name;
 
-        file.mv("./uploads/"+filename, (error) => {
-            if(error)
-                return res.status(400).send({message: error});
-            else
-                return res.send({result: ImportExcel(filename), message: "File Upload Success."});
+    const path = require("path");
+    if (path.extname(filename) != ".xlsx" && path.extname(filename) != ".csv")
+      return res
+        .status(400)
+        .send({ message: "Invalid file type. Please use a .xlsx file." });
+
+    file.mv("./uploads/" + filename, error => {
+      if (error) return res.status(400).send({ message: error });
+      else
+        return res.send({
+          result: ImportExcel(filename),
+          message: "File Upload Success."
         });
-    }
+    });
+  }
 });
 
 // Get Voter with LRN
-router.post('/searchVoter/', async (req, res) => {
-    const voter = await Voter.find(req.body.param).populate('voteReceiptID')
-        .catch((error) => {
-            return res.send(error.message);
-        });
+router.post("/searchVoter/", async (req, res) => {
+  const findArg = req.body.param;
 
-    res.send(voter);
+  for (let k in req.body.param) {
+    switch (k) {
+      case "fullName":
+        findArg.fullName = {
+          $regex: new RegExp(".*" + findArg.fullName + ".*"),
+          $options: "i"
+        };
+        break;
+      case "section":
+        findArg.section = {
+          $regex: new RegExp(".*" + findArg.section + ".*"),
+          $options: "i"
+        };
+        break;
+    }
+  }
+
+  const voter = await Voter.find(findArg)
+    .populate("voteReceiptID")
+    .catch(error => {
+      return res.send(error.message);
+    });
+
+  res.send(voter);
 });
 
 //Get the total Number of Voters that already Voted
-router.get('/totalVoted/:gradeLevel', async (req, res) => {
-    let totalVoters = 0;
-    
-    if(req.params.gradeLevel == "all"){
-        totalVoters = await Voter.countDocuments({voteReceiptID: {$ne:null}});
-    }else{
-        totalVoters = await Voter.countDocuments(
-            {voteReceiptID: {$ne:null},
-            gradeLevel: req.params.gradeLevel},
-        );
-    }
+router.get("/totalVoted/:gradeLevel", async (req, res) => {
+  let totalVoters = 0;
 
-    res.send({totalVoters: totalVoters});
+  if (req.params.gradeLevel == "all") {
+    totalVoters = await Voter.countDocuments({ voteReceiptID: { $ne: null } });
+  } else {
+    totalVoters = await Voter.countDocuments({
+      voteReceiptID: { $ne: null },
+      gradeLevel: req.params.gradeLevel
+    });
+  }
+
+  res.send({ totalVoters: totalVoters });
 });
 
 //Get the total Number of Voters
-router.get('/totalVoters/:gradeLevel', async(req, res) => {
-    let totalVoters = 0;
-    
-    if(req.params.gradeLevel == "all")
-        totalVoters = await Voter.countDocuments();
-    else
-        totalVoters = await Voter.countDocuments({gradeLevel: req.params.gradeLevel});
+router.get("/totalVoters/:gradeLevel", async (req, res) => {
+  let totalVoters = 0;
 
-    res.send({totalVoters: totalVoters});
+  if (req.params.gradeLevel == "all")
+    totalVoters = await Voter.countDocuments();
+  else
+    totalVoters = await Voter.countDocuments({
+      gradeLevel: req.params.gradeLevel
+    });
+
+  res.send({ totalVoters: totalVoters });
 });
 
 // Activate Voter with Lrn
-router.put('/activateVoter/:lrn', [auth, admin], async (req, res) => {
-    let voter = await Voter.findOne({lrn: parseInt(req.params.lrn)});
+router.put("/activateVoter/:lrn", [auth, admin], async (req, res) => {
+  let voter = await Voter.findOne({ lrn: parseInt(req.params.lrn) });
 
-    if(voter.voteReceiptID)
-        return res.status(400).send('Voter has already voted. Sorry...');
-        
-    if(!voter)
-        return res.status(404).send('Voter was not found from the database...');
+  if (voter.voteReceiptID)
+    return res.status(400).send("Voter has already voted. Sorry...");
 
-    voter.canVote = !voter.canVote;
+  if (!voter)
+    return res.status(404).send("Voter was not found from the database...");
 
-    voter = await voter.save()
-        .then(result => res.send(result))
-        .catch(error => res.send(error));
+  voter.canVote = !voter.canVote;
+
+  voter = await voter
+    .save()
+    .then(result => res.send(result))
+    .catch(error => res.send(error));
 });
 
 // Auth Voter
-router.post('/auth', async(req,res) => {
-    const { error } = validate(req.body);
-    if (error) res.status(400).send(error);
+router.post("/auth", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) res.status(400).send(error);
 
-    let voter = await Voter.findOne(_.pick(req.body, ['lrn']));
-    
-    if(!voter) 
-        return res.status(404).send({message: `Voter not found.`});
-    
-    if(!voter.canVote)
-        return res.status(403).send({message: 'Sorry, voter cannot vote yet. Contact admin for more info.'});
+  let voter = await Voter.findOne(_.pick(req.body, ["lrn"]));
 
-    if(voter.voteReceiptID)
-        return res.status(403).send({message: 'Voter has already voted.'});
-    
-    voterFromDB = _.pick(voter, ['fullName', 'gradeLevel','lrn','section']);
-    voterFromDB.fullName = voterFromDB.fullName.replace(/ /g,'').toLowerCase();
-    voterFromDB.section = voterFromDB.section.replace(/ /g,'').toLowerCase();
-    console.log(voterFromDB);
+  if (!voter) return res.status(404).send({ message: `Voter not found.` });
 
-    if(!_.isEqual(req.body, voterFromDB))
-        return res.status(400).send('Invalid Voter Details.');
+  if (!voter.canVote)
+    return res.status(403).send({
+      message: "Sorry, voter cannot vote yet. Contact admin for more info."
+    });
 
-    const token = generateToken();
-    res.send({authToken: token});
+  if (voter.voteReceiptID)
+    return res.status(403).send({ message: "Voter has already voted." });
+
+  voterFromDB = _.pick(voter, ["fullName", "gradeLevel", "lrn", "section"]);
+  voterFromDB.fullName = voterFromDB.fullName.replace(/ /g, "").toLowerCase();
+  voterFromDB.section = voterFromDB.section.replace(/ /g, "").toLowerCase();
+  console.log(voterFromDB);
+
+  if (!_.isEqual(req.body, voterFromDB))
+    return res.status(400).send("Invalid Voter Details.");
+
+  const token = generateToken();
+  res.send({ authToken: token });
 });
 
-function ImportExcel(filename){
-    const excelToJsonResult = excelToJson({
-        sourceFile: './uploads/' + filename,
-        header: {
-            rows: 1
-        },
-        columnToKey:{
-            A: 'lrn',
-            B: 'fullName',
-            C: 'gradeLevel',
-            D: 'section'
-        }
-    });
+function ImportExcel(filename) {
+  const excelToJsonResult = excelToJson({
+    sourceFile: "./uploads/" + filename,
+    header: {
+      rows: 1
+    },
+    columnToKey: {
+      A: "lrn",
+      B: "fullName",
+      C: "gradeLevel",
+      D: "section"
+    }
+  });
 
-    excelToJsonResult.Sheet1.forEach(async (element) => {
-        console.log(element);
-        if(!validate(element))
-            return console.error(`Voter Schema invalid.`);
-        
-        let voter = await Voter.findOne({lrn: element.lrn});
+  excelToJsonResult.Sheet1.forEach(async element => {
+    console.log(element);
+    if (!validate(element)) return console.error(`Voter Schema invalid.`);
 
-        if(voter != null){
-            console.log(`Skipped - Voter already in the database: ${voter.lrn}`);
-            return;
-        }
+    let voter = await Voter.findOne({ lrn: element.lrn });
 
-        voter = new Voter(_.pick(element,
-            ['lrn', 'fullName', 'gradeLevel', 'section']));
+    if (voter != null) {
+      console.log(`Skipped - Voter already in the database: ${voter.lrn}`);
+      return;
+    }
 
-        await voter.save();
+    voter = new Voter(
+      _.pick(element, ["lrn", "fullName", "gradeLevel", "section"])
+    );
 
-        console.log(`Success - Voter was added to the database: ${voter.lrn}`);
-    });
+    await voter.save();
 
-    return excelToJsonResult;
+    console.log(`Success - Voter was added to the database: ${voter.lrn}`);
+  });
+
+  return excelToJsonResult;
 }
 
 module.exports = router;
